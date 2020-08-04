65d7fdba36feec128c5e66b3b37f3c86bead4545
David Honegger Rogers <dhr@lanl.gov>
2020-08-04 16:27:27 -0600

    adding browser/server test scripts

---
+++ testing/browser.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+
+
+webbrowser.open_new("http://127.0.0.1:8000/cinema_explorer.html")
+webbrowser.open_new_tab("http://127.0.0.1:8000/cinema_explorer.html?database=data/sphere.cdb")
+webbrowser.open_new_tab("http://127.0.0.1:8000/cinema_explorer.html?databases=small.json")
+
